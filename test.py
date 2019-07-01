7299a8e464c739d29046b60f86b96c1815a9fd6b
EC2 Default User <ec2-user@ip-172-31-91-143.ec2.internal>
2019-07-01 00:18:52 +0000

    added name and instance state

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ import boto3
 session = boto3.Session(profile_name='awsservices')
 ec2 = session.resource('ec2')
 
-for i in ec2.instances.all(): print(i)
+for i in ec2.instances.all(): 
+    print(i.public_ip_address, i.state)
