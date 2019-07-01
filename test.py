6c2b0baf05de23ba19791d51fa5c6c7901a0b00c
EC2 Default User <ec2-user@ip-172-31-91-143.ec2.internal>
2019-07-01 00:37:14 +0000

    Update to use click for command line options

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import boto3
+import click
 
 session = boto3.Session(profile_name='awsservices')
 ec2 = session.resource('ec2')
 
-for i in ec2.instances.all(): 
-    print(i.public_ip_address, i.state)
+@click.command()
+def list_instance():
+    "List EC2 Instances"
+    for i in ec2.instances.all(): 
+        print(i.id, i.instance_type, i.public_ip_address, i.state['Name'])
+    return
+
+if __name__ == '__main__':
+    list_instance()
+    
